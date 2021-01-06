562009c573b4c7c805db70c95ae7b92f4f185db3
Abdul Fatir <a.fatir@yahoo.com>
2021-01-06 17:15:25 +0000

    Added whoami resource

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req, res) => {
+  res.json({ ipaddress: req.ip, language: req.get('Accept-Language'), software: req.get('User-Agent') });
+});
 
 
 // listen for requests :)
